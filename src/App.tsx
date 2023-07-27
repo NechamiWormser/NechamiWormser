621d76d439ceda1d7f314cc505dec47cefd2f893
Nechami Wormser <n0548507345@gmail.com>
2023-07-27 23:54:46 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    // <div className="App">
+    <header className="App-header">
+      <div>
+        Let's Talk App!!!
+      </div>
+      
+    </header>
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn n
+    //     </a>
+    //   </header>
+
+  );
+}
+
+export default App;
